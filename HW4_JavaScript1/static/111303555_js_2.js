2ce28bfa6b23c6e0093372791f3438cb345145b6
hsiang <bkong3269@gmail.com>
2025-09-25 15:37:51 +0800

    新增()和=的符號然後寫紀錄符號的函數

--- HW4_JavaScript1/static/111303555_js_2.js
+++ HW4_JavaScript1/static/111303555_js_2.js
@@ -2,6 +2,10 @@ document.write('<h1>計算機</h1>');
 document.write(`<input id="expr" type="text" placeholder="輸入算式，如：6*(8+8)">`);
 document.write(`<div class="calc">`);
 
+function appendChar(ch) {
+  document.getElementById("expr").value += ch;
+}
+
 let buf = `<div class="row">`;
 for (let n = 0; n <= 9; n++) {
   // 每列 3 個：0 1 2 / 3 4 5 / 6 7 8，最後一列 9 + clear
@@ -21,6 +25,8 @@ document.write(`<div class="row">` +
                `<button class="btn" onclick="appendChar('-')">-</button>` +
                `<button class="btn" onclick="appendChar('*')">*</button>` +
                `<button class="btn" onclick="appendChar('/')">/</button>` +
+               `<button class="btn" onclick="appendChar('(')">(</button>` +
+               `<button class="btn" onclick="appendChar(')')">)</button>` +
+               `<button class="btn wide" onclick="calcExpr()">=</button>` +
                `</div>`);
 
-               
