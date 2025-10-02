461df6b9bd235b91933d5c533b80ef505ad446e7
hsiang <bkong3269@gmail.com>
2025-10-02 15:55:17 +0800

    修改秒數顯示與計時方法，完成作業5-1

--- HW5_JavaScript2/static/111303555_js_1.js
+++ HW5_JavaScript2/static/111303555_js_1.js
@@ -1,36 +1,60 @@
 let answer = Math.floor(Math.random() * 100) + 1;
 let attempts = 0;
-let timerId = null;
+let startTime = null; 
 
 function startTimer() {
-    let seconds = 0;
+    startTime = Date.now();
     const timerElement = document.getElementById("times");
-    return setInterval(() => {
-        seconds ++;
+
+    timerInterval = setInterval(() => {
+        let seconds = getElapsedSeconds();
         timerElement.textContent = `已經過時間: ${seconds} 秒`;
-    }, 1000);       
+    }, 1000);
 }
-function stopTimer(timer) {
-    clearInterval(timer);
+
+function stopTimer() {
+    clearInterval(timerInterval);
+    timerInterval = null;
 }
+
+function getElapsedSeconds() {
+    if (!startTime) return 0;
+    return Math.floor((Date.now() - startTime) / 1000);
+}
+
+
 function onGuess(){
-    if (attempts === 0 && !timerId) {
-        timerId = startTimer();
+
+    if (attempts === 0 && !startTime) {
+        startTimer(); 
     }
+
     const guessField = document.getElementById("guessField");
     const guess = Number(guessField.value);
     attempts++;
 
     if (guess === answer) {
-        stopTimer(timerId);
-        alert(`恭喜你猜對了！答案是${answer}。你總共猜了${attempts}次。`);
+
+        let seconds = getElapsedSeconds();
+        let list = document.getElementById("list");
+        let record = document.createElement("p");
+
+        record.textContent = `猜了${attempts}次，耗時${seconds}秒，時間${new Date().toLocaleTimeString()}`;
+        list.appendChild(record);
+
+        alert(`恭喜你猜對了！答案是${answer}。你總共猜了${attempts}次，花了${seconds}秒`);
         attempts = 0; 
         answer = Math.floor(Math.random() * 100) + 1;
         guessField.value = '';
+        startTime = null;
+        document.getElementById("times").textContent = "秒數:"
+        document.getElementById("message").textContent = "";
     } else if (guess < answer) {
         document.getElementById("message").textContent = "太小了，再試一次！";
     } else if (guess > answer) {   
         document.getElementById("message").textContent = "太大了，再試一次！";
     }
 }
+
+
 
