7651d439fd926e8ab2fd18cd1872d021cc4b3262
hsiang <bkong3269@gmail.com>
2025-10-03 23:27:38 +0800

    完成結帳按鈕按下時total金額>0，跳出結帳明細 按下時total金額<=0，不作動作

--- HW5_JavaScript2/static/111303555_js_2.js
+++ HW5_JavaScript2/static/111303555_js_2.js
@@ -7,7 +7,7 @@ function checkqty (inputId) {
         } else {
         input.value = num; 
     }
-    update()
+    updatetotal()
     }
 
 
@@ -22,7 +22,7 @@ function increase (inputId, qtyId) {
         } else {
             input.value = qty
         }
-    update()
+    updatetotal()
     }
 
 function decrease (inputId) {
@@ -32,19 +32,52 @@ function decrease (inputId) {
         a --    
     }   
     document.getElementById(inputId).value = a;
-    update()
+    updatetotal()
 }
-function update() {
+function updatetotal() {
     calculate("melonsnum", "melonprice", "melontotal");
     calculate("banananum", "bananaprice", "bananatotal");
     calculate("RTXnum", "RTXprice", "RTXtotal");
-    
+
 }
 function calculate(numId, priceId, totalId) {
     let qty = parseInt(document.getElementById(numId).value);
     let price = parseInt(document.getElementById(priceId).textContent.replace("$", ""));
     let total = qty * price;
     document.getElementById(totalId).textContent = "$" + total;
+}
+
+function checkout() {
+    let checkboxes = document.querySelectorAll(".product");
+    let total = 0;
+
+    checkboxes.forEach(cb => {
+        let qtyId = cb.getAttribute("data_qty");
+        let totalId = cb.getAttribute("data_total");
+        let qty = parseInt(document.getElementById(qtyId).value) || 0;
+        let subtotal = parseInt(document.getElementById(totalId).textContent.replace("$", "")) || 0;
+
+    if (cb.checked) {
+        total += subtotal;
+    } else {
+        document.getElementById(qtyId).value = 0;
+        document.getElementById(totalId).textContent = "$0";
+    }
+});
+    if (total <= 0){
+        return;
+    }
+    document.getElementById("total").textContent = "$" + total;
+
+    alert("感謝您的購買，您購買的產品如下:"
+    + "\n西瓜: " + document.getElementById("melonsnum").value + " 顆"
+    + "\n香蕉: " + document.getElementById("banananum").value + " 根"
+    + "\nRTX5070: " + document.getElementById("RTXnum").value + " 張"
+    + "\n總金額: " + document.getElementById("total").textContent); 
+
+    checkboxes.forEach(cb => {
+        cb.checked = false;
+    })
 }
 
 // function melloncalculate() {
